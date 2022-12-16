2cfed9b0bff08f7f6041f83578844a9ba6d57e18
tanguybernard <bernardtanguy1pro@gmail.com>
2022-12-16 14:28:44 +0100

    add twig

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,15 @@
 <?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Config\Twig;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
@@ -29,5 +33,25 @@ $app->get('/template-with-ob-start', function(Request $request, Response $respon
     $response->getBody()->write($content);
     return $response;
 });
+
+
+$app->get('/form', function (Request $request, Response $response, $args) {
+    $twig = Twig::get();
+    $response->getBody()->write($twig->render('form.html.twig', ['name' => 'John Doe',
+        'occupation' => 'gardener']));
+
+    return $response;
+});
+
+$app->post('/myform', function (Request $request, Response $response, $args) {
+
+    $form = $request->getParsedBody();//[name]
+    //var_dump($form);
+    //var_dump($form['name']);
+    //var_dump($form['email']);
+
+    return $response;
+});
+
 
 $app->run();
