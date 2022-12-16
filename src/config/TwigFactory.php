42993f22a508f733d2fa391ed801c52ee97b0708
tanguybernard <bernardtanguy1pro@gmail.com>
2022-12-16 14:40:59 +0100

    rename Twig to TwigFactory

--- src/config/TwigFactory.php
+++ src/config/TwigFactory.php
@@ -4,7 +4,7 @@ namespace Config;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-class Twig {
+class TwigFactory {
     public static function get() : Environment{
         $loader = new FilesystemLoader(__DIR__ . '/../templates');
         return new Environment($loader);
